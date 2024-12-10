52f65a1007b26d1c5711c259abcc6464e3d7a806
Piyush Choudhari <piyushchoudhari503@gmail.com>
2024-12-10 13:16:08 +0530

    Initialize project with React and basic configuration

---
+++ src/MyComponents/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      This is an about component
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum minima labore provident omnis eligendi, ab, doloremque corporis necessitatibus vitae dignissimos recusandae nam ea atque quae deserunt! Dolorem vitae porro iure expedita. Alias, harum repudiandae?</p>
+    </div>
+  )
+}
+
+export default About
